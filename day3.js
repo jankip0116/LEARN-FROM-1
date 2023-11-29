03d3a5c623a666db365ea943c0693ec52112af14
jankip0116 <jankip1223@gmail.com>
2023-11-29 14:39:13 +0530

    seconf commit

--- day3.js
+++ day3.js
@@ -1,3 +1,8 @@
-<script>
-    
-</script>
+function innertextdemo(){
+    let a = document.getElementById('p1')
+    a. innerText = "this is inner text <b>example</b>";
+}
+function innerHTMLdemo(){
+    let b = document.getElementById('p1');
+    b.innerHTML = "this is inner HTML<b>example</b>";
+}
